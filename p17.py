8381b02a381438db3bf7f86fce5cdd19b6b5efc3
AstroB20 <bhisman20@gmail.com>
2024-12-19 16:35:06 +0530

    tek tek

--- p17.py
+++ p17.py
@@ -1,5 +1,5 @@
 def letterCombinations(digits:str):
-    val={"2":"abc","3":'def',"4":'ghi',"5":'jkl',"6":'mno','7':'pqrs','8':'tuv',9:'wxyz'}
+    val={"2":"abc","3":'def',"4":'ghi',"5":'jkl',"6":'mno','7':'pqrs','8':'tuv',"9":'wxyz'}
     lsStr=list(digits)
     res=[]
 
