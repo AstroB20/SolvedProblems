a6591e1f583a1d0246b75e8e56bb89ad797bda91
AstroB20 <bhisman20@gmail.com>
2025-05-19 11:10:28 +0530

    Refactor threeSumClosest function to improve efficiency and readability

--- p16.py
+++ p16.py
@@ -1,14 +1,22 @@
-def threeSumClosest(nums, target):
-    n=len(nums)
-    res=[0,0,0]
-    minDiff=float('inf')
-    for i in range(n - 2):
-        for j in range(i + 1, n - 1):
-            for k in range(j + 1, n):
-                currSum = nums[i] + nums[j] + nums[k]
-                currDiff = abs(currSum - target)
-                if currDiff < minDiff:
-                    minDiff = currDiff
-                    res = [nums[i], nums[j], nums[k]]
-    return sum(res)
-print(threeSumClosest([-1,2,1,-4],1))
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        closest_sum = float('inf')
+        for i in range(len(nums) - 2):
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                current_sum = nums[i] + nums[left] + nums[right]
+                if abs(current_sum - target) < abs(closest_sum - target):
+                    closest_sum = current_sum
+                if current_sum < target:
+                    left += 1
+                elif current_sum > target:
+                    right -= 1
+                else:
+                    return current_sum
+        return closest_sum
