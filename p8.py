c19e6562717b5bc6d6530cc95253be8a0dddb452
AstroB20 <bhisman20@gmail.com>
2025-05-24 22:03:48 +0530

    Add solutions for removing the N-th node from the end of a linked list, generating a spiral matrix, and implementing permutations of an array

---
+++ p8.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.strip()
+        if not s:
+            return 0
+        sign = 1
+        if s[0] == '-':
+            sign = -1
+            s = s[1:]
+        elif s[0] == '+':
+            s = s[1:]
+        result = 0
+        for char in s:
+            if not char.isdigit():
+                break
+            result = result * 10 + int(char)
+        result *= sign
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+        if result < INT_MIN:
+            return INT_MIN
+        if result > INT_MAX:
+            return INT_MAX
+        return result
