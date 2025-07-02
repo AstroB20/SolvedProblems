8e50b90f2c9fde47739ad78041a91e46bbefbb75
AstroB20 <bhisman20@gmail.com>
2025-07-02 22:30:25 +0530

    Refactor possibleStringCount to simplify dynamic programming approach

--- p3333.py
+++ p3333.py
@@ -13,24 +13,19 @@ class Solution(object):
             j = i
             while j < n and word[j] == word[i]:
                 j += 1
-            runs.append((word[i], j - i))
+            runs.append(j - i)
             i = j
         
         num_runs = len(runs)
-        max_length = len(word)
+        dp = [0] * (n + 1)
+        dp[0] = 1
         
-        dp = [ [0] * (max_length + 1) for _ in range(num_runs + 1) ]
-        dp[0][0] = 1
-        
-        for i in range(1, num_runs + 1):
-            char, run_length = runs[i-1]
-            for l in range(max_length + 1):
-                for t in range(1, run_length + 1):
-                    if l - t >= 0:
-                        dp[i][l] = (dp[i][l] + dp[i-1][l - t]) % MOD
-        result = 0
-        for l in range(k, max_length + 1):
-            result = (result + dp[num_runs][l]) % MOD
-        
-        return result
-        
+        for run_len in runs:
+            new_dp = [0] * (n + 1)
+            for s in range(n + 1):
+                if dp[s]:
+                    for t in range(1, run_len + 1):
+                        if s + t <= n:
+                            new_dp[s + t] = (new_dp[s + t] + dp[s]) % MOD
+            dp = new_dp
+        return sum(dp[k:]) % MOD
