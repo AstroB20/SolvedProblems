157d3302b4e64c7b38455824f16fc1ac0407bc03
AstroB20 <bhisman20@gmail.com>
2025-07-20 18:39:50 +0530

    Add implementation of isBalanced method to check if a binary tree is height-balanced

---
+++ p110.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return checkBalance(root) != -1;
+    }
+
+    private int checkBalance(TreeNode node) {
+        if (node == null)
+            return 0;
+
+        int leftHeight = checkBalance(node.left);
+        int rightHeight = checkBalance(node.right);
+
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
