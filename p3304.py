73eefd8bc4164c726149a0ede1b5672988c8daef
AstroB20 <bhisman20@gmail.com>
2025-08-16 22:41:30 +0530

    Remove implementation of kthCharacter method and retain example usage

--- p3304.py
+++ p3304.py
@@ -1,15 +1,4 @@
-class Solution(object):
-    def kthCharacter(self, k):
-        """
-        :type k: int
-        :rtype: str
-        """
-        word = "a"
-        while len(word)<k:
-            for alpha in word:
-                word+=chr(ord(alpha)+1)
-            print(word)
-        return word[k-1] if k <= len(word) else "a"
+
 # Example usage:
 sol = Solution()
 print(sol.kthCharacter(5))  # Output: 'e'
