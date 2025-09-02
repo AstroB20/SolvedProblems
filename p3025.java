64dc09d6d99a7e1bf39ad62eb97c12edb8425a6e
AstroB20 <bhisman20@gmail.com>
2025-09-02 23:02:44 +0530

    Add implementation of numberOfPairs method to count valid point pairs

---
+++ p3025.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numberOfPairs(int[][] points) {
+        int n = points.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int ax = points[i][0], ay = points[i][1];
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    continue;
+                int bx = points[j][0], by = points[j][1];
+                if ((ax != bx || ay != by) && ax <= bx && ay >= by) {
+                    boolean empty = true;
+                    for (int k = 0; k < n; k++) {
+                        if (k == i || k == j)
+                            continue;
+                        int x = points[k][0], y = points[k][1];
+                        if (x > ax && x < bx && y < ay && y > by) {
+                            empty = false;
+                            break;
+                        }
+                    }
+                    if (empty)
+                        count++;
+                }
+            }
+        }
+        return count;
+    }
+}
