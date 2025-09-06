c9c07efe3b28d05dd2ecdeeb2e0366b21b216c3b
AstroB20 <bhisman20@gmail.com>
2025-09-06 17:17:05 +0530

    Refactor twoSum and groupAnagrams methods for improved efficiency and clarity

--- p49.java
+++ p49.java
@@ -1,13 +1,26 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> anagramMap = new HashMap<>();
-        for (String str : strs) {
-            char[] charArray = str.toCharArray();
-            Arrays.sort(charArray);
-            String sortedStr = new String(charArray);
-            anagramMap.putIfAbsent(sortedStr, new ArrayList<>());
-            anagramMap.get(sortedStr).add(str);
+        if (strs.length == 0) {
+            return new ArrayList();
         }
-        return new ArrayList<>(anagramMap.values());
+        Map<String, List> ansMap = new HashMap<>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append("#");
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!ansMap.containsKey(key)) {
+                ansMap.put(key, new ArrayList());
+            }
+            ansMap.get(key).add(s);
+        }
+        return new ArrayList(ansMap.values());
     }
 }
