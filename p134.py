eee22da87406cd1947e8b8d92873a7482e28ce05
AstroB20 <bhisman20@gmail.com>
2025-12-24 16:45:05 +0530

    Add implementation of canCompleteCircuit method to determine the starting gas station for a circular route

---
+++ p134.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        total_gas = 0
+        current_gas = 0
+        start = 0
+        for i in range(len(gas)):
+            diff = gas[i] - cost[i]
+            total_gas += diff
+            current_gas += diff
+            if current_gas < 0:
+                start = i + 1
+                current_gas = 0
+        return start if total_gas >= 0 else -1
